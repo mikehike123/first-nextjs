3e5e33a47eee3f3e5acde9cbb37a6341a494c272
mikehike123@gmail.com <mikehike123@gmail.com>
2025-03-07 19:34:28 -0800

    Added Google Analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,31 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { useEffect } from "react";
+
+declare global {
+  interface Window {
+    dataLayer: any[];
+    gtag: any;
+  }
+}
 
 export default function App({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    // Add the GA4 script to the head
+    const script = document.createElement("script");
+    script.async = true;
+    script.src = `https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_ID}`;
+    document.head.appendChild(script);
+
+    // Initialize GA4
+    window.dataLayer = window.dataLayer || []; // Initialize dataLayer if it doesn't exist
+    function gtag(...args: any[]) {
+      window.dataLayer.push(args);
+    }
+    gtag("js", new Date());
+    gtag("config", process.env.NEXT_PUBLIC_GA_ID);
+
+  }, []);
+
   return <Component {...pageProps} />;
 }
